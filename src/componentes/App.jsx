587029868ee394883fdf71f8d1fff8de9d57ee54
Dennis <dennispaixaoads@gmail.com>
2021-10-16 17:13:28 -0300

    children

--- src/componentes/App.jsx
+++ src/componentes/App.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 import Primeiro from './Primeiro';
 import Fragmento from './Fragmento';
 import Card from './layout/Card';
+import Familia from './Familia';
+import FamiliaMembro from './FamiliaMembro';
 
 export default props => {
     return (
         <div className="CardContainer">
+            <Card titulo="Children">
+                 <Familia sobrenome="Silva">
+                     <FamiliaMembro nome="joao" />
+                     <FamiliaMembro nome="Sabrina" />
+                </Familia>    
+            </Card>
+
             <Card titulo="Primeiro" bgcolor="aqua"> <Primeiro /> </Card>
             <Card titulo="Fragmento" bgcolor="yellow"> <Fragmento /></Card>
             <Card titulo="Primeiro" > <Primeiro /> </Card>
---
+++ src/componentes/FamiliaMembro.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default (props)=>{
+    return(
+
+       
+            <p> {props.nome} <strong>{props.sobrenome}</strong></p>
+        
+
+    )
+}
